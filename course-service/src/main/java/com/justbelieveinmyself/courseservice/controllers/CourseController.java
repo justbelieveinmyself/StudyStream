f9792b1b7ede1de9d40054e46e892b7bd8812d2c
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2024-04-06 23:47:36 +0300

    Course-Service: CourseController: refactor all annotations to coursecontroller interface

--- course-service/src/main/java/com/justbelieveinmyself/courseservice/controllers/CourseController.java
+++ course-service/src/main/java/com/justbelieveinmyself/courseservice/controllers/CourseController.java
@@ -2,7 +2,6 @@ package com.justbelieveinmyself.courseservice.controllers;
 
 import com.justbelieveinmyself.courseservice.domains.dtos.CourseDto;
 import com.justbelieveinmyself.courseservice.domains.dtos.update.UpdateCourseDto;
-import com.justbelieveinmyself.courseservice.services.CourseService;
 import com.justbelieveinmyself.library.aspects.ValidateErrors;
 import com.justbelieveinmyself.library.dto.ResponseMessage;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,29 +9,17 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/v1/course")
-@CrossOrigin
-@RequiredArgsConstructor
 @Tag(name = "Course API", description = "Create, get, update and delete courses")
 @SecurityRequirement(name = "Bearer Authentication")
-public class CourseController {
-    private final CourseService courseService;
+public interface CourseController {
 
     @Operation(summary = "Get Course by ID", description = "Get Course by ID")
     @GetMapping("/{courseId}")
-    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long courseId) {
-        CourseDto courseDto = courseService.getCourseById(courseId);
-        // 1. representation from dtos? embedded? ex. course includes a lot of modules, modules contains lessons, lessons contains a lot of questions etc..
-        // 2. service must returns responseentity or dto?
-        return ResponseEntity.ok(courseDto);
-    }
+    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long courseId);
 
     @Operation(summary = "Create Course", description = "Create Course")
     @PostMapping
@@ -41,20 +28,14 @@ public class CourseController {
             @RequestBody @Valid CourseDto courseDto,
             BindingResult result,
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId
-    ) {
-        CourseDto savedCourseDto = courseService.createNewCourse(courseDto, userId);
-        return new ResponseEntity<>(savedCourseDto, HttpStatus.CREATED);
-    }
+    );
 
     @Operation(summary = "Delete Course by ID", description = "Delete Course by ID")
     @DeleteMapping("/{courseId}")
     public ResponseEntity<ResponseMessage> deleteCourseById(
             @PathVariable Long courseId,
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId
-    ) {
-        courseService.deleteCourseById(courseId, userId);
-        return ResponseEntity.noContent().build();
-    }
+    );
 
     @Operation(summary = "Update Course by ID", description = "Update Course by ID")
     @PutMapping("/{courseId}")
@@ -64,10 +45,7 @@ public class CourseController {
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId,
             @RequestBody @Valid UpdateCourseDto updateCourseDto,
             BindingResult result
-    ) {
-        CourseDto courseDto = courseService.updateCourseById(courseId, userId, updateCourseDto);
-        return ResponseEntity.ok(courseDto);
-    }
+    );
 
     @Operation(summary = "Partial Update Course by ID", description = "Partial Update Course by ID")
     @PatchMapping("/{courseId}")
@@ -76,9 +54,6 @@ public class CourseController {
             @Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId,
             @RequestBody UpdateCourseDto updateCourseDto,
             BindingResult result
-    ) {
-        CourseDto courseDto = courseService.patchCourseById(courseId, userId, updateCourseDto);
-        return ResponseEntity.ok(courseDto);
-    }
+    );
 
 }
